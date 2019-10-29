5285250768b93d3fa1917563e2bd0dac29e240a2
Vlad <53820373+catprogrammer-cat@users.noreply.github.com>
2019-10-29 12:34:10 +0200

    Create lab8_1.cpp

---
+++ lab8_1.cpp
@@ -0,0 +1,56 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //declare array 'a[3][5]'
+    int a[3][5];
+    //input entire array 'a[3][5]'
+    for(int i = 0; i<=2; i++){
+        for(int j = 0; j<=4; j++){
+            cin >> a[i][j];
+        } 
+    }
+    //output entire array 'a[3][5]' 
+    for(int i = 0; i<=2; i++){
+        cout << i << " : "; 
+        for(int j = 0; j<=4; j++){
+            cout << a[i][j] << " ";
+        }
+        cout << ";" << endl;
+    }
+    //change negative element to zero(0)
+    for(int i = 0; i<=2; i++){
+        for(int j = 0; j<=4; j++){
+            if(a[i][j]  < 0){
+                a[i][j] = 0;
+            }
+        }
+    }
+    cout << "after manipulation" << endl;
+    //output entire array 'a[3][5]' 
+    for(int i = 0; i<=2; i++){
+        cout << i << " : "; 
+        for(int j = 0; j<=4; j++){
+            cout << a[i][j] << " ";
+        }
+        cout << ";" << endl;
+    }
+    return 0;
+}
+//result: 0 : 34 -23 34 54 23 ;                                                                                                           
+//        1 : 65 -34 -54 34 23 ;                                                                                                          
+//        2 : -67 12 32 24 -34 ;                                                                                                          
+//        after manipulation                                                                                                              
+//        0 : 34 0 34 54 23 ;                                                                                                             
+//        1 : 65 0 0 34 23 ;                                                                                                              
+//        2 : 0 12 32 24 0 ;
