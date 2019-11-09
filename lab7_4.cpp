72bf4e67e92a30b59f3661c3816cca1c73b95909
Vlad <53820373+catprogrammer-cat@users.noreply.github.com>
2019-11-09 18:45:30 +0200

    Update lab7_4.cpp

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -3,8 +3,12 @@
 //3×5
 //Integer
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
+
+
 
 void output_quantity_odd_and_replace_corner_of_quantity_odd(int x, int y){
         int i,j;
@@ -56,20 +60,65 @@ void output_quantity_odd_and_replace_corner_of_quantity_odd(int x, int y){
         }
     }
     
+    //3×5
+    void fill_randomly_array2d(int array2d[][5]){
+        srand ( time(0) );
+        for (int i=0; i<3; i++){
+            for (int j=0; j<5; j++){
+                array2d[i][j] = rand() % 100 + 1; 
+            }
+        }
+    }
+
+    void print_array2d(int array2d[][5]) {
+        for(int i = 0; i<3; i++){
+            cout << i << " : "; 
+            for(int j = 0; j<5; j++){
+                cout << array2d[i][j] << " ";
+            }
+            cout << ";" << endl;
+        }
+    }
+    
+    int sum_odd_element_array2d(int array2d[][5]){
+        int sum = 0;
+        for( int i=0; i<3; i++)
+        {
+            for(int j=0; j<5; j++)
+            {
+                if(array2d[i][j] % 2 != 0 && array2d[i][j] > 0)
+                {
+                    sum += array2d[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+        
+    
+    
 int main()
 {
-    
-    output_quantity_odd_and_replace_corner_of_quantity_odd(4,5);
+    //3×5
+    int array2d[3][5];
+    fill_randomly_array2d(array2d);
+    print_array2d(array2d);
 
+    cout << "Sum of odd element is " << sum_odd_element_array2d(array2d) << endl;
+    
+    array2d[0][0] = sum_odd_element_array2d(array2d);
+    array2d[0][4] = sum_odd_element_array2d(array2d);
+    array2d[2][0] = sum_odd_element_array2d(array2d);
+    array2d[2][4] = sum_odd_element_array2d(array2d);
+    
+    print_array2d(array2d);
+    
     return 0;
 }
-// 0 : 3 4 34 5 4 ;                                                                                                                                                          
-// 1 : 5 6 5 4 3 ;                                                                                                                                                           
-// 2 : 4 5 4 32 1 ;                                                                                                                                                          
-// 3 : 5 4 3 2 5 ;                                                                                                                                                           
-// quantity_odd is 10                                                                                                                                                        
-// 0 : 10 4 34 5 10 ;                                                                                                                                                        
-// 1 : 5 6 5 4 3 ;                                                                                                                                                           
-// 2 : 4 5 4 32 1 ;
-// 2 : 4 5 4 32 1 ;                                                                                                                                                          
-// 3 : 10 4 3 2 10 ;
+// 0 : 84 61 96 45 8 ;                                                                                               
+// 1 : 20 48 17 92 77 ;                                                                                              
+// 2 : 96 43 55 2 2 ;                                                                                                
+// Sum of odd element is 298                                                                                         
+// 0 : 298 61 96 45 298 ;                                                                                            
+// 1 : 20 48 17 92 77 ;                                                                                              
+// 2 : 298 43 55 2 298 ; 
